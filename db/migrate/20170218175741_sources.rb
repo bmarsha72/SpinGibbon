73c102d18821d1a4415a44bc868f07d6e4ebb853
brian <bmarsha72@gmail.com>
2017-02-18 12:27:22 -0600

    all migrations

--- db/migrate/20170218175741_sources.rb
+++ db/migrate/20170218175741_sources.rb
@@ -1,4 +1,9 @@
 class Sources < ActiveRecord::Migration[5.0]
   def change
+    create_table :sources do |table|
+      table.string :source
+      table.string :rss
+      table.string :category
+    end
   end
 end
--- db/migrate/20170218175908_accounts.rb
+++ db/migrate/20170218175908_accounts.rb
@@ -1,4 +1,14 @@
 class Accounts < ActiveRecord::Migration[5.0]
   def change
+    create_table :accounts do |table|
+      table.string :first_name
+      table.string :last_name
+      table.string :email
+      table.string :password_hash
+      table.string :password_salt
+      table.string :liberal
+      table.string :moderate
+      table.string :conservative
+  end
   end
 end
